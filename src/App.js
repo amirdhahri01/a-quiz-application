48724b560dc0b8b5c239602c95388a3035e6b1f4
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-25 23:09:21 +0100

    SetupForm component is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import SetupForm from "./Form/SetupForm";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <SetupForm />
+    </>
   );
 }
 
--- src/Form/SetupForm.jsx
+++ src/Form/SetupForm.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const SetupForm = () => {
   return (
-    <div>SetupForm</div>
-  )
-}
+    <main>
+      <div className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          <div className="form-control">
+            <label htmlFor="amount">number</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              className="form-input"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">category</label>
+            <select name="category" id="category" className="form-input">
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="difficulty">difficulty</label>
+            <select name="difficulty" id="difficulty">
+              <option value="easy">easy</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+          </div>
+          <p className="error">Can't generates questinos, please try again !</p>
+          <button type="submit" className="submit-btn">
+            start
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-export default SetupForm
+export default SetupForm;
