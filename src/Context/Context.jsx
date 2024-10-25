bb152b64a2fab71d48d807e443227a6089ea38a9
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-25 23:44:23 +0100

    An update

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react"
+import { useContext, useState, createContext } from "react";
 
-const AppProvider = () => {
-  const [waiting , setWainting] = useState(true);
-  const [loading , setLoading] = useState(false);
-  const [questions ,setQuestions] = useState([]);
-  const [index , setIndex] = useState(0);
-  const [correct , setCorrect] = useState(0);
-  const [error , setError] = useState(false);
-  const [quiz , setQuiz] = useState({
-    amount:10,
-    category:"sports",
-    difficulty:"ease"
+const AppContext = createContext();
+
+const AppProvider = ({Children}) => {
+  const [waiting, setWainting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: "sports",
+    difficulty: "ease",
   });
-  return <>
+  const [modal, setModal] = useState(false);
   
-  </>
-}
-export default AppProvider
+  return <AppContext.Provider>{Children}</AppContext.Provider>;
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export {AppContext , AppProvider};
