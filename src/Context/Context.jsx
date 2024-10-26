75abc8cb9c66d95f87e1ac533b427bfef9ca0d89
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-26 21:54:44 +0100

    fetchQuestions method is done

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { useContext, useState, createContext } from "react";
 
 const AppContext = createContext();
 
-const AppProvider = ({Children}) => {
+const AppProvider = ({ Children }) => {
   const [waiting, setWainting] = useState(true);
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
@@ -15,11 +16,33 @@ const AppProvider = ({Children}) => {
     difficulty: "ease",
   });
   const [modal, setModal] = useState(false);
-  
+
+  const fetchQuestions = async () => {
+    setLoading(false);
+    setWainting(false);
+    const response = await axios
+      .get("https://opentdb.com/api.php?amount=10")
+      .catch((err) => console.log(err.message()));
+    if (response) {
+      const data = response.data.results;
+      if (data.length) {
+        setQuestions(data);
+        setLoading(false);
+        setWainting(false);
+        setError(false);
+      } else {
+        setWainting(true);
+        setLoading(true);
+      }
+    } else {
+      setWainting(true);
+    }
+  };
+
   return <AppContext.Provider>{Children}</AppContext.Provider>;
 };
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
 
-export {AppContext , AppProvider};
+export { AppContext, AppProvider };
